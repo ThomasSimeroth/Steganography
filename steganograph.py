e42f46a068e93541827f0aea5a5800f536efd2fe
Thomas Simeroth <thomassimeroth@gmail.com>
2018-12-01 19:18:30 -0600

    First pass at encoding

--- steganograph.py
+++ steganograph.py
@@ -1,9 +1,20 @@
 from PIL import Image
-im = Image.open("example.jpg")
-width, height = im.size
-st = "Hello!"
-print(' '.join(format(ord(x), 'b') for x in st))
-for i in range(width):
-    for j in range(height):
-        pixel = im.getpixel((i,j))
-        #print(pixel)
+
+def encodeMessage(path, message):
+    im = Image.open(path)
+    width, height = im.size
+    newImage = im.copy()
+    pixels = newImage.load()
+    byteString = ''.join(format(ord(x), 'b') for x in message)
+    messageSize = len(byteString)
+    if height < messageSize or width < messageSize:
+        return None
+    else:
+        for i in range(len(byteString)):
+            red, green, blue = im.getpixel((i,i))
+            red += int(byteString[i])
+            pixels[i, i] = (red, green, blue)
+        newImage.save("example2.jpg", "png")
+        return newImage
+
+encodeMessage("example.jpg", "Hello!")
