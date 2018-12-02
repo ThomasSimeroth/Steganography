ba1b53018b76f7af8ff2ef56dd92c186798c0d87
Thomas Simeroth <thomassimeroth@gmail.com>
2018-12-01 18:38:58 -0600

    How to encode string into binary

--- steganograph.py
+++ steganograph.py
@@ -1,8 +1,9 @@
 from PIL import Image
 im = Image.open("example.jpg")
 width, height = im.size
+st = "Hello!"
+print(' '.join(format(ord(x), 'b') for x in st))
 for i in range(width):
     for j in range(height):
         pixel = im.getpixel((i,j))
-        print(pixel)
-
+        #print(pixel)
