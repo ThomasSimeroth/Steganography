475e8ecd102b591b834fe7254b529ef0ecc006e6
Domenic Cusanelli <stl3339@gmail.com>
2018-12-02 20:42:26 -0600

    text files work

--- image_aes.py
+++ image_aes.py
@@ -10,18 +10,29 @@ def encrypt(filename, key):
 
     with open(filename, 'rb') as infile:
         with open(encrypted_file, 'wb') as outfile:
-            chunk = infile.read(AES_BLOCK_SIZE).encode("utf8")
+            chunk = infile.read(AES_BLOCK_SIZE)
             while len(chunk) > 0:
-                outfile.write(aes_encryptor.encrypt(pad(chunk, AES_BLOCK_SIZE)))
-                chunk = infile.read(AES_BLOCK_SIZE).encode("utf8")
+                if len(chunk) % 16 == 0:
+                    outfile.write(aes_encryptor.encrypt(chunk))
+                else:
+                    outfile.write(aes_encryptor.encrypt(pad(chunk, AES_BLOCK_SIZE)))
+                
+                chunk = infile.read(AES_BLOCK_SIZE)
 
     return init_vector
 
-def decrypt(filename, key, init_vector):
+def decrypt(filename, key, init_vector): 
+    AES_BLOCK_SIZE = 16
+
+    aes_decryptor = AES.new(key, AES.MODE_CBC, init_vector)
     decrypted_file = "new_" + filename[10:]
-        
+
     with open(filename, 'rb') as infile:
         with open(decrypted_file, 'wb') as outfile:
+            chunk = infile.read(AES_BLOCK_SIZE)
+            while len(chunk) > 0:
+                outfile.write(aes_decryptor.decrypt(chunk))
+                chunk = infile.read(AES_BLOCK_SIZE)
             
 
 def generate_key(key_text):
@@ -30,6 +41,10 @@ def generate_key(key_text):
     return key_text[:16].encode("utf8")
 
 def main():
-    
+    file_to_e = "testtext"
+    file_to_d = "encrypted_testtext"
+    key = "password"
+    init_vector = encrypt(file_to_e, generate_key(key))
+    decrypt(file_to_d, generate_key(key), init_vector)
 
 main()
